e5a32422edd5c3ed07fcc2e6c2adbb71698ce540
mehdithreem <mehdithreem@gmail.com>
2017-12-05 17:08:16 +0330

    befor forwarding test

--- hazard_Detection_Unit.v
+++ hazard_Detection_Unit.v
@@ -2,9 +2,9 @@ module hazard_Detection_Unit(
 	input[4:0] src1,
 	input[4:0] src2,
 	input[4:0] Exe_Dest,
-	input Exe_WB_EN,
+	input Exe_MEM_R,
 	input[4:0] Mem_Dest,
-	input Mem_WB_EN,
+	input Mem_MEM_R,
 	
 	input is_immediate,
 	input st_or_bne,
@@ -18,18 +18,18 @@ always@(*) begin
 	hazard_reg = 0;
 	
 	if (is_immediate == 0) begin
-		if (Exe_WB_EN) begin
+		if (Exe_MEM_R) begin
 			if (src1 == Exe_Dest || src2 == Exe_Dest)
 				hazard_reg = 1;
 		end
-		if (Mem_WB_EN) begin
-			if (src1 == Mem_Dest || src2 == Mem_Dest)
-				hazard_reg = 1;
-		end
+//		if (Mem_MEM_R) begin
+//			if (src1 == Mem_Dest || src2 == Mem_Dest)
+//				hazard_reg = 1;
+//		end
 	end
 	
 	if (is_immediate == 1) begin
-		if (Exe_WB_EN) begin
+		if (Exe_MEM_R) begin
 			if (src1 == Exe_Dest)
 				hazard_reg = 1;
 			
@@ -39,9 +39,9 @@ always@(*) begin
 			end
 		end
 		
-		if (Mem_WB_EN) begin
-			if (src1 == Mem_Dest)
-				hazard_reg = 1;
+		if (Mem_MEM_R) begin
+//			if (src1 == Mem_Dest)
+//				hazard_reg = 1;
 			
 			if (st_or_bne) begin
 				if (src2 == Mem_Dest)

7e38933278543b18f0c69fa55a126f4ab8ec5901
Nahal <mirzanahal@gmail.com>
2017-12-19 19:02:24 +0330

    SRAM_controller added

--- SRAM_Controller.v
+++ SRAM_Controller.v
@@ -7,15 +7,15 @@ module SRAM_Controller(
 	input SRAM_re_en,
 	input SRAM_we_en,
 	output [31:0] SRAM_read_data, // DONE
-	output reg ready,
+	output ready,
 	// TO THE SRAM PINS
-	inout reg [15:0] SRAM_DATA, // DONE
-	output reg [17:0] SRAM_ADDRESS, // DONE
-	output reg SRAM_UB_N_O, //GND
-	output reg SRAM_LB_N_O, //GND
-	output reg SRAM_WE_N_O, // DONE
-	output reg SRAM_CE_N_O, //GND
-	output reg SRAM_OE_N_O //GND
+	inout [15:0] SRAM_DATA, // DONE
+	output [17:0] SRAM_ADDRESS, // DONE
+	output SRAM_UB_N_O, //GND
+	output SRAM_LB_N_O, //GND
+	output SRAM_WE_N_O, // DONE
+	output SRAM_CE_N_O, //GND
+	output SRAM_OE_N_O //GND
 );
 
 	reg[1:0] counter = 2'b0;
@@ -29,6 +29,9 @@ module SRAM_Controller(
 		else begin
 			counter = 2'b0;
 		end
+		
+		first_part = (SRAM_re_en && counter == 2'b0) ? SRAM_DATA : first_part;
+		second_part = (SRAM_re_en && counter == 2'b01) ? SRAM_DATA : second_part;
 	end
 	
 	assign SRAM_WE_N_O = (SRAM_we_en && (counter == 2'b0 || counter == 2'b01));
@@ -39,10 +42,7 @@ module SRAM_Controller(
 				(SRAM_we_en && counter == 2'b01) ? SRAM_write_data[31:16]:
 				32'bZ;
 
-	assign first_part = (SRAM_re_en && counter == 2'b0) ? SRAM_DATA : first_part;
-	assign second_part = (SRAM_re_en && counter == 2'b01) ? SRAM_DATA : second_part;
-
-	assign SRAM_read_data = {second_part, first_part};
+	assign SRAM_read_data = {second_part, second_part};
 
 	assign ready = (SRAM_we_en || SRAM_re_en) && counter != 2'b11 ? 0 : 1;
 
